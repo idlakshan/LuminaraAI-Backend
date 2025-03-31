0422e595060ebe0961ffdcce73233b2e67390dec
idlakshan <id.lakshan21@gmail.com>
2025-03-31 12:17:11 +0530

    feat: initialize project with Domain-Driven Architecture

---
+++ src/api/hotel.js
@@ -0,0 +1,12 @@
+import express from "express"
+import { createHotel, deleteHotel, getAllHotels, getHotelById, updateHotel } from "../application/hotel.js";
+
+const hotelsRouter=express.Router();
+
+hotelsRouter.get("/",getAllHotels);
+hotelsRouter.get("/:id",getHotelById);
+hotelsRouter.post("/",createHotel);
+hotelsRouter.put("/:id",updateHotel);
+hotelsRouter.delete("/:id",deleteHotel);
+
+export default hotelsRouter;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+import "dotenv/config"
 import express from "express";
+import hotelsRouter from "./api/hotel.js";
+import connectDB from "./infrastructure/db.js";
 
 const app= express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    console.log("GET request received");
-    return res.status(200).json("value");
- });
+connectDB();
+
+app.use("/api/hotels",hotelsRouter)
 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
